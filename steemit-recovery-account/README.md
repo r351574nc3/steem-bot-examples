0a7d011bcf24a6635bfdd88ae33a7b801b2265b9
Leo Przybylski <r351574nc3@gmail.com>
2018-02-27 23:14:11 -0700

    Adding project for changing the recovery account

---
+++ steemit-recovery-account/README.md
@@ -0,0 +1,9 @@
+# Steemit Recovery Account
+
+Example source code for changing your recovery account
+
+## Usage
+
+```
+npm run build ; npm run recovery
+```
---
+++ steemit-recovery-account/src/config/index.ts
@@ -0,0 +1,2 @@
+export let user = process.env.STEEM_NAME || "YOU NEED TO FILL THIS IN ICEHOLE";
+export let wif = process.env.STEEM_WIF || "YOU NEED TO FILL THIS IN ICEHOLE";
