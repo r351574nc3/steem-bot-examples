e9e5ec764c03f87914b2069bde189e98894c6350
Leo Przybylski <r351574nc3@gmail.com>
2018-03-24 10:53:01 -0700

    Adding voting and comments queues. Refinements to buffered reading of EXIF data.

--- steem-exif-spider-bot/helpers/bot/index.js
+++ steem-exif-spider-bot/helpers/bot/index.js
@@ -1,6 +1,27 @@
+const voting_queue = [];
+const comment_queue = [];
+
+const voting = {
+    length: () => { return voting_queue.length },
+    push: (obj) => { return voting_queue.push(obj) },
+    pop: () => { return voting_queue.pop() },
+    shift: () => { return voting_queue.shift() },
+    unshift: (obj) => { return voting_queue.unshift(obj) }
+}
+
+const comments = {
+    length: () => { return comment_queue.length },
+    push: (obj) => { return comment_queue.push(obj) },
+    pop: () => { return comment_queue.pop() },
+    shift: () => { return comment_queue.shift() },
+    unshift: (obj) => { return comment_queue.unshift(obj) }
+}
+
 
 function run() {
-    return require("./exif").execute();
+    require('./comment').execute(comments)
+    require('./vote').execute(voting)
+    require('./exif').execute(voting, comments)
 }
 
 
---
+++ steem-exif-spider-bot/helpers/bot/vote.js
@@ -0,0 +1,84 @@
+const Promise = require('bluebird')
+const steem = require('steem')
+const { user, wif, weight } = require('../../config')
+const schedule = require('node-schedule')
+const moment = require('moment');
+
+const MINUTE = new schedule.RecurrenceRule();
+MINUTE.second = 1
+
+const SECONDS_PER_HOUR = 3600
+const PERCENT_PER_DAY = 20
+const HOURS_PER_DAY = 24
+const MAX_VOTING_POWER = 10000
+const DAYS_TO_100_PERCENT = 100 / PERCENT_PER_DAY
+const SECONDS_FOR_100_PERCENT = DAYS_TO_100_PERCENT * HOURS_PER_DAY * SECONDS_PER_HOUR
+const RECOVERY_RATE = MAX_VOTING_POWER / SECONDS_FOR_100_PERCENT
+const DEFAULT_THRESHOLD = 9500
+
+
+function current_voting_power(vp_last, last_vote) {
+    console.log("Comparing %s to %s ", moment().utc().add(7, 'hours').local().toISOString(), moment(last_vote).utc().local().toISOString())
+
+    var seconds_since_vote = moment().utc().add(7, 'hours').local().diff(moment(last_vote).utc().local(), 'seconds')
+    return (RECOVERY_RATE * seconds_since_vote) + vp_last
+}
+
+function time_needed_to_recover(voting_power, threshold) {
+    return (threshold - voting_power) / RECOVERY_RATE
+}
+
+function check_can_vote() {
+    return steem.api.getAccountsAsync([ user]).then((accounts) => {
+        if (accounts && accounts.length > 0) {
+            const account = accounts[0];
+            console.log("Voting threshold for %s: %s", user, DEFAULT_THRESHOLD)
+            console.log("Getting voting power for %d %s", account.voting_power, account.last_vote_time)
+            var voting_power = current_voting_power(account.voting_power, account.last_vote_time)
+            if (voting_power > DEFAULT_THRESHOLD) {
+                return true;
+            }
+        }
+        return false;
+    })
+}
+
+function vote(author, permlink, weight) {
+    return steem.broadcast.voteAsync(
+        wif, 
+        user, 
+        author,
+        permlink,
+        weight
+    )
+    .then((results) =>  {
+        console.log("Vote results: ", results)
+        return results;
+    },
+    (err) => {
+        console.log("Vote failed for %s: %s", user, err.message)
+    })
+}
+
+function execute(voting) {
+    schedule.scheduleJob(MINUTE, function() {
+        if (voting.length() < 1) {
+            return {};
+        }
+               
+        const { author, permlink, weight } = voting.shift();
+
+        return check_can_vote().then((can_vote) => {
+            if (can_vote) {
+                vote(author, permlink, weight)
+            }
+            else {
+                voting.push({ author, permlink, weight })
+            }
+        })
+    })
+}
+
+module.exports = {
+    execute
+}
