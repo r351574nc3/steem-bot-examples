ec31ecb90cb2f8567ddc0e173e83a8dca3a22940
Leo Przybylski <r351574nc3@gmail.com>
2018-03-30 11:37:43 -0700

    Adding beneficiary support

--- steem-exif-spider-bot/config/index.js
+++ steem-exif-spider-bot/config/index.js
@@ -2,6 +2,7 @@ module.exports = {
     user: process.env.STEEM_NAME,
     wif: process.env.STEEM_WIF,
     weight: parseInt(process.env.VOTING_WEIGHT), 
+    bennies: process.env.STEEM_BENEFICIARIES ? process.env.STEEM_BENEFICIARIES.split(",") : [],
     blacklist: [
         "emwalker",
         "tomlabe",
