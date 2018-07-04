58a897a6dc17d4fb0f46cfb96930087d55530aee
Leo Przybylski <r351574nc3@gmail.com>
2018-07-03 21:47:03 -0700

    UPdating curation bot

--- curation-trail-bot/index.js
+++ curation-trail-bot/index.js
@@ -5,10 +5,103 @@ const moment = require('moment')
 const fs = require('fs')
 
 const voting_queue = [];
+const ONE_SECOND = 1000
 const FIVE_SECONDS = 5000
 const TEN_MINUTES = 600000
 const THIRTY_MINUTES = 1800000
 
+const blacklist = [
+    "iqbalel",
+    "jhonysins",
+    "khabir",
+    "tombredy",
+    "catonchronic",
+    "adelsz",
+    "timemaster",
+    "shahinalom",
+    "sjeezz",
+    "mdfaysal",
+    "maycoded",
+    "soudy-jr74",
+    "kyawmyoaung",
+    "soomraa",
+    "fatany",
+    "delowar4181",
+    "hormorhk18",
+    "soumenz",
+    "nahidcom",
+    "sultanam",
+    "smhp84",
+    "smhp2016",
+    "trbtc",
+    "josegaldame",
+    "funny3dkids2",
+    "saidqautsar",
+    "josephace135",
+    "steem.moon",
+    "markgreek",
+    "karthikmbbs",
+    "jemisteem",
+    "doctorspence",
+    "haikal21",
+    "rashidnazirmir",
+    "nurainiagani",
+    "andreina89",
+    "kgakakillerg",
+    "tipsybosphorus",
+    "ajpacheco1610",
+    "albertocotua",
+    "mundocreativo",
+    "adip",
+    "dwightjaden",
+    "hatakekakashi",
+    "princesammer3366",
+    "najuz",
+    "riamisna",
+    "goldenaardvark",
+    "chiquibencomo",
+    "plimpd",
+    "leob1234",
+    "dexterflux",
+    "fauzict",
+    "aduwahsp",
+    "olamatto",
+    "nazmul.islam",
+    "stefidifelice",
+    "miguelalar"
+]
+
+const follow = [
+    "frontrunner",
+    "sahra-bot"
+]
+
+const allowed_tags = [
+    "utopian-io",
+    "task-development",
+    "task-graphics",
+    "task-bug-hunting",
+    "task-social",
+    "task-analysis",
+    "task-documentation",
+    "task-copywriting",
+    "ideas",
+    "blog",
+    "tutorials",
+    "video-tutorials",
+    "graphics",
+    "development",
+    "bug-hunting",
+    "analysis",
+    "social",
+    "documentation",
+    "copywriting"
+]
+
+instant_voters = [
+    'smartmarket',
+    'minnowbooster'
+]
 
 steem.api.setOptions({ url: 'wss://rpc.buildteam.io' });
 
@@ -39,14 +132,15 @@ function url_to_post(url) {
 function processTransfer(transfer) {
     const amount = parseFloat(transfer.amount.split(" ").shift())
 
-    if (amount >= 30) {
+    if (amount >= 25) {
         console.log("Found valid transfer ", transfer)
         return url_to_post(transfer.memo)
             .spread((author, permlink) => {
                 return steem.api.getContentAsync(author, permlink)
                     .then((content) => {
                         const age_in_seconds = moment().utc().local().diff(moment(content.created).utc().local(), 'seconds')
-                        const wait_time = 1801 - age_in_seconds
+                        const wait_time = instant_voters.includes(transfer.to) && age_in_seconds > 0 ? (1801 - age_in_seconds) * 1000 : 0
+                        console.log(`Queueing for ${wait_time} milliseconds`)
                         setTimeout(() => {
                             voting_queue.push({ author, permlink })
                         }, wait_time) 
@@ -66,32 +160,51 @@ function processComment(comment) {
                 setTimeout(() => {
                     voting_queue.push({ author: comment.author, 
                                         permlink: comment.permlink, 
-                                        weight: whitelist[comment.author].weight })
+                                        weight: whitelist[comment.author].weight,
+                                        whitelisted: true })
                 }, THIRTY_MINUTES)
                 return comment
             }
             return steem.api.getContentAsync(comment.author, comment.permlink)
                 .then((content) => {
-                    if (content.json_metadata && content.json_metadata != '') {
+                    if (content.json_metadata 
+                        && content.json_metadata != ''
+                        && content.parent_permlink == 'utopian-io') {
                         return JSON.parse(content.json_metadata);
                     }
                     return {};
                 })
                 .then((metadata) => {
-                    if (metadata.tags && metadata.tags.includes('utopian-io')) {
+                    if (metadata.tags && metadata.tags.length > 0 && metadata.tags.includes("utopian-io")) {
+                        return metadata.tags
+                    }
+                    return []
+                })
+                .filter((tag) => allowed_tags.includes(tag))
+                .then((tags) => {
+                    if (tags && tags.length > 1) {
                         setTimeout(() => {
                             voting_queue.push({ author: comment.author, permlink: comment.permlink })
                         }, THIRTY_MINUTES)
                     }
-                    return metadata
+                    return tags;
                 })
         })
 }
 
-function list_voters() {
+function list_voters(author, permlink) {
     const voters = JSON.parse(fs.readFileSync(process.env.CONFIG_DIR + "/voters.json"));
-    return Promise.map(voters, (item, index, length) => {
-        return item
+    return Promise.filter(voters, (voter, index, length) => {
+        if (!(author && permlink)) {
+            return true
+        }
+
+        // Filter promises by checking if the voter name is among the active voters
+        return steem.api.getActiveVotesAsync(author, permlink)
+            .map((vote) => vote.voter)
+            .then((target) => {
+                return !target.includes(voter.name)
+            })
     })
 }
 
@@ -100,12 +213,26 @@ function list_whitelist() {
     return Promise.resolve(retval)
 }
 
+function list_blacklist() {
+    return Promise.all(blacklist)
+}
+
 function vote(post) {
     if (!post) {
         return Promise.reject("Invalid post")
     }
 
-    return list_voters()
+    return list_blacklist()
+        .filter((member) => member === post.author)
+        .then((blacklist) => {
+            console.log(`Checking if ${post.author} in blacklist`)
+            if (blacklist.length > 0) {
+                return []
+            }
+
+            return list_voters(post.author, post.permlink)
+        })
+        .filter((voter) => (!post.whitelisted || !voter.skip_whitelist))
         .map((voter) => {
             console.log("Upvoting ", post)
             console.log("Voter ", voter.name)
@@ -114,6 +241,12 @@ function vote(post) {
                 .then((results)  => {
                     console.log("Vote results ", results)
                     return results;
+                })
+                .catch((err) => {
+                    console.log("Voting error ", err)
+                    if (err.payload.indexOf("STEEMIT_MIN_VOTE_INTERVAL_SEC") > -1) {
+                        voting_queue.push(post)
+                    }
                 })
         })
 }
@@ -137,6 +270,9 @@ function main() {
                     }
                     break;
                 case 'vote':
+                    if (operation.weight > 0 && follow.includes(operation.voter)) {
+                        return vote({ author: operation.author, permlink: operation.permlink })
+                    }
                     break;
                 case 'unvote':
                     break;
@@ -144,6 +280,7 @@ function main() {
                     return processTransfer(operation)
                     break;
                 default:
+                    break;
             }
         })
         .catch((err) => {
@@ -153,9 +290,9 @@ function main() {
 }
 
 setInterval(() => {
-    vote(voting_queue.pop())
+    const to_vote = voting_queue.shift()
+    vote(to_vote)
         .catch((err) => {
-
         })
-}, FIVE_SECONDS)
+}, ONE_SECOND)
 main()
